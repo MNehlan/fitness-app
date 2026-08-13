4372adc1f09274b20238c04c80c1c1f23f9639f8
MNehlan <211360561+MNehlan@users.noreply.github.com>
2026-08-13 16:58:48 +0530

    Add workout creation flow

---
+++ client/src/components/WorkoutForm.tsx
@@ -0,0 +1,49 @@
+import { useState, type ChangeEvent, type SubmitEvent } from 'react';
+import type { Workout } from '../types/workout';
+
+type WorkoutFormProps = {
+  onAddWorkout: (workout: Workout) => void;
+};
+
+const WorkoutForm = ({ onAddWorkout }: WorkoutFormProps) => {
+  const [newWorkout, setNewWorkout] = useState({
+    name: '',
+  });
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    setNewWorkout((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  }
+
+  function handleSubmit(e: SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!newWorkout.name.trim()) {
+      return;
+    }
+
+    const workout: Workout = {
+      id: crypto.randomUUID(),
+      name: newWorkout.name,
+    };
+
+    onAddWorkout(workout);
+    console.log(workout);
+    setNewWorkout({
+      name: '',
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        name='name'
+        value={newWorkout.name}
+        onChange={handleChange}
+      />
+      <button type='submit'>Add</button>
+    </form>
+  );
+};
+
+export default WorkoutForm;
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import WorkoutSummary from '../components/WorkoutSummary';
 import WorkoutList from '../components/WorkoutList';
 import type { Workout } from '../types/workout';
+import WorkoutForm from '../components/WorkoutForm';
 
 const Dashboard = () => {
   const [workouts, setWorkouts] = useState<Workout[]>([
@@ -15,12 +16,17 @@ const Dashboard = () => {
     },
   ]);
 
+  function addWorkout(workout: Workout): void {
+    setWorkouts((prev) => [...prev, workout]);
+  }
+
   return (
     <div>
       <h1>Welcome Back</h1>
       <p>Today's workout</p>
       <WorkoutSummary workouts={workouts} />
       <WorkoutList workouts={workouts} />
+      <WorkoutForm onAddWorkout={addWorkout} />
     </div>
   );
 };
