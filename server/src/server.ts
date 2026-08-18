522b21df82d67f39153d87d5e00e05dff2eac965
MNehlan <211360561+MNehlan@users.noreply.github.com>
2026-08-18 19:11:38 +0530

    Add PostgreSQL integration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,30 @@
 import express from 'express';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import 'dotenv/config';
+import pool from './db.js';
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.get('/', (req, res) => {
-  res.send('Fitness API is running')
+app.get('/', async (_req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Database error');
+  }
 });
+
+app.get('/api/exercises', async (req, res) => {
+  try {
+    const data = await pool.query('select * from exercises')
+    res.json(data.rows)
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Database error')
+  }
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
